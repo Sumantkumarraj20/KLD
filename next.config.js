2953f70d373158b84a9ace17fdfcf24675251f6a
sumantkumarraj20@gmail.com <sumantkumarraj20@gmail.com>
2026-02-08 21:19:32 +0530

    deployment issues

--- next.config.js
+++ next.config.js
@@ -1,16 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   reactStrictMode: true,
   compress: true,
   poweredByHeader: false,
   generateEtags: true,
-  pageExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  pageExtensions: ["ts", "tsx", "js", "jsx"],
   typescript: {
-    tsconfigPath: './tsconfig.json'
+    tsconfigPath: "./tsconfig.json",
   },
   productionBrowserSourceMaps: false,
   staticPageGenerationTimeout: 60,
-  
+
   // Image optimization
   images: {
     unoptimized: true,
